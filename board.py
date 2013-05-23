784f7fd871daaadbc40fc5b47dc5b690f32e507e
Alex Kowalczuk <askowalczuk93@gmail.com>
2013-05-23 17:11:27 -0500

    add docstrings, fixed indentation

--- board.py
+++ board.py
@@ -1,30 +1,50 @@
+"""
+Class that
+defines board that will be used used for
+the Game of Life. 
+
+Includes all methods to update, show, and change state of board.
+"""
+
 import sys
 import copy
 
 class board(object):
-  def __init__(self, size):
-      self.size = size
-      self.b = []
-      for i in range(0, size):
-          self.b.append([])
-      for y in range(0, size):
-        for x in range(0, size):
-          self.b[x].append('.')
+    """
+    Constructor for the board.  Takes a simple parameter: size.
+    Size determines the size of the board.  Board will be a square of this size
+    """
+    def __init__(self, size):
+        self.size = size
+        self.b = []
+        for i in range(0, size):
+            self.b.append([])
+        for y in range(0, size):
+          for x in range(0, size):
+            self.b[x].append('.')
 
   def show(self):
+      """
+      Shows board by printing to terminal 
+      Invoked automatically or with 'show' command ingame
+      """
       for y in range(0, self.size):
           for x in range(0, self.size):
               sys.stdout.write(self.b[x][y] + ' ')
           sys.stdout.write('\n')
 
   def show_with_coords(self):
+      """ 
+      Shows board with coordinates, useful for setting specific coordinates.
+      Invoked with 'coordinates' command ingame
+      """
       sys.stdout.write("  ")
       for i in range(0, self.size):
+          # Keep spacing consistent, include a space for single digits
           if i < 10:
               sys.stdout.write(str(i) + " ")
           elif i >=10:
               sys.stdout.write(str(i))
-
       sys.stdout.write("\n")
 
       for y in range(0, self.size):
@@ -38,28 +58,49 @@ class board(object):
           sys.stdout.write('\n')
 
   def set(self, x, y):
+      """ Set a space on the board.  Invoked with 'set' ingame """
       try:
           self.b[x][y] = '#'
       except:
           print "Not a valid set command"
+
   def unset(self, x, y):
+      """ Unset a space on the board. Invoked with 'unset' ingame """
       try:
           self.b[x][y] = '.'
       except:
           print "Not a valid unset command"
+
   def update(self):
+      """
+      Main update routine for board.  Will set cells according to ruleset.
+      Invoked with 'update' or simple by pressing enter when update mode is on.
+      Rules adjustable by adjusting constants below, makes for some fun.
+      """
+      # TODO: add methods to adjust the ruleset ingame
+      OVERPOP = 3
+      UNDERPOP = 2
+      SURVIVAL = 3
+
       buf = copy.deepcopy(self.b)
       for y in range(0, self.size):
-        for x in range(0, self.size):
-          living = self.living_neighbors(x,y)
-          if self.b[x][y] == '.' and living == 3 or living == 4:
-              buf[x][y] = '#'     
-          elif self.b[x][y] == '#':
-            if living > 3 or living < 2:
-              buf[x][y] = '.'
-      self.b = buf
+          for x in range(0, self.size):
+              living = self.living_neighbors(x,y)
+                  if self.b[x][y] == '.' and living == SURVIVAL:
+                      buf[x][y] = '#' 
+
+                  elif self.b[x][y] == '#':
+                      if living > OVERPOP or living < UNDERPOP:
+                          buf[x][y] = '.'
+          self.b = buf
 
   def living_neighbors(self, x, y):
+      """
+      Gets living neighbors for a particular cell (ie: cells with value '#').
+      """
+
+      # There maybe a simpler way to implement this, it turned out this way
+      # was easier than adding a padding of invisible empty cells around the board
       live = 0
 
       try:
--- game.py
+++ game.py
@@ -1,25 +1,43 @@
+"""
+Class that defines the I/O methods for starting and running the Game of Life.
+To start game:
+    $ python game.py 
+You will be prompted to provide the size of the board, then can start playing!
+"""
+
 import board as b
 import sys
 
 class game(object):
+    """ Constructor for the game class.  Makes a simple game with a board """
     def __init__(self):
+        # Is putting more than assignments in the constructor bad practice?
+        # I couldn't think of better way to do it...
         self.game_size = input("What size would you like to play? >> ")
         self.board = b.board(self.game_size)
 
     def play(self):
+        """
+        Method that will allow you to play the game! 
+        By default, the board will be shown at the end of the loop.  
+        Use update mode for rapid updating
+        """
         update_mode = False
         self.board.show()
+
         while True:
             std_show = True
             action = raw_input("> ")
 
+            # TODO: Change this to a switch statement, probably easier
+
 
             if "set" in action:
-              a = action.split()
-              self.board.set(int(a[1]), int(a[2]))
+                a = action.split()
+                self.board.set(int(a[1]), int(a[2]))
             if "unset" in action:
-              a = action.split()
-              self.board.unset(int(a[1]), int(a[2]))
+                a = action.split()
+                self.board.unset(int(a[1]), int(a[2]))
 
             elif action == "update" or action == "u":
                 self.board.update()
